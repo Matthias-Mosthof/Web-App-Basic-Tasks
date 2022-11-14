c40255bc442a186b5e6773810a8f97444d92e1a0
Matthias Mosthof <matthias.mosthof@gmail.com>
2022-11-14 10:17:26 +0100

    adds-js-functions-and-statemgmnt

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,3 +1,12 @@
+const btnAdd = document.querySelector("#btn-add");
+const inpAdd = document.querySelector("#inp-add");
+
+const rmvBtn = document.querySelector("#btn-remove");
+
+const btnAll = document.querySelector("#all");
+const btnOpen = document.querySelector("#open");
+const btnDone = document.querySelector("#done");
+
 const state = {
   todos: [
     { description: "Learn HTML", done: true },
@@ -8,8 +17,7 @@ const state = {
 function renderTodos() {
   const list = document.querySelector("#list");
   list.innerHTML = "";
-
-  state.todos.forEach((todo) => {
+  state.todos.forEach((todo, index, arr) => {
     const todoLi = document.createElement("li");
 
     const checkbox = document.createElement("input");
@@ -19,6 +27,34 @@ function renderTodos() {
     checkbox.addEventListener("change", (event) => {
       const newToDoDoneState = event.target.checked;
       todo.done = newToDoDoneState;
+      if (newToDoDoneState === true) {
+        todoLi.style.textDecoration = "line-through";
+      } else todoLi.style.textDecoration = "none";
+    });
+
+    btnDone.addEventListener("change", (evnt) => {
+      console.log(btnDone.checked);
+      if (btnDone.checked === true) {
+        if (todo.done != true) {
+          todoLi.style.display = "none";
+        } else if (btnAll.checked === true || btnDone.checked === true) {
+          todoLi.style.display = "";
+        }
+      }
+    });
+
+    btnOpen.addEventListener("change", (eventt) => {
+      if (btnOpen.checked === true) {
+        if (todo.done != false) {
+          todoLi.style.display = "none";
+        } else if (btnAll.checked === true || btnOpen.checked === true) {
+          todoLi.style.display = "";
+        }
+      }
+
+      btnAll.addEventListener("change", () => {
+        todoLi.style.display = "";
+      });
     });
 
     todoLi.appendChild(checkbox);
@@ -31,3 +67,40 @@ function renderTodos() {
 }
 
 renderTodos();
+
+btnAdd.addEventListener("click", (e) => {
+  state.todos.push({ description: inpAdd.value, done: false });
+  inpAdd.value = "";
+  renderTodos();
+});
+
+rmvBtn.addEventListener("click", (ev) => {
+  state.todos.forEach((todo, index, arr) => {
+    if (todo.done === true) {
+      arr.splice(index, 1);
+      renderTodos();
+    }
+  });
+});
+
+let lastCheckedItem;
+let clickCounter = 0;
+
+// Funktion das nur eine Radiobox aktiv sein kann
+function onlyOneSelect(event) {
+  if (event.target.checked === true) {
+    clickCounter++;
+  } else {
+    clickCounter--;
+  }
+
+  if (clickCounter === 2) {
+    lastCheckedItem.checked = false;
+    clickCounter--;
+  }
+  lastCheckedItem = event.target;
+}
+// Aufruf der Funktion
+btnAll.addEventListener("change", onlyOneSelect);
+btnDone.addEventListener("change", onlyOneSelect);
+btnOpen.addEventListener("change", onlyOneSelect);
