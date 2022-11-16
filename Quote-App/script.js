c4e3347a14c2d9343d16a83dfc4a6bdc83cb1625
Matthias Mosthof <matthias.mosthof@gmail.com>
2022-11-16 13:47:15 +0100

    small-changes

--- Quote-App/script.js
+++ Quote-App/script.js
@@ -1,25 +1,20 @@
 const getQuoteBtn = document.querySelector("#get-quote");
-const dynamicQuote = document.querySelector("#dyn-quote");
+const quote = document.querySelector("#quote");
 const author = document.querySelector("#author");
 
 getQuoteBtn.addEventListener("click", loaddata);
 
 function loaddata() {
-  dynamicQuote.innerText = "";
-  author.innerText = "";
   fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
     .then((response) => {
-      console.log("ok");
       return response.json();
     })
 
     .then((data) => {
       let quoteText = data.data[0].quoteText;
-      let authorOne = data.data[0].quoteAuthor;
+      let authorText = data.data[0].quoteAuthor;
 
-      let quote = document.createTextNode(quoteText);
-
-      dynamicQuote.append(quote);
-      author.append("-" + authorOne);
+      quote.innerText = quoteText;
+      author.innerText = authorText;
     });
 }
